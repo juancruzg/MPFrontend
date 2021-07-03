3c4ef120c10bb5cdba6774d823c1afec8213f50a
Juan Grasso <jgrasso@primerosystems.com>
2021-07-03 16:35:15 -0300

    Tab-spacing consistency in client app

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import Search from './components/search';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/">
-          <Search></Search>
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route path="/">
+                    <Search></Search>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
